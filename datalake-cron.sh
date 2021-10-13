981eda27e713f8a608a0111cf13e7f326910ac09
Łukasz Gryglicki <lgryglicki@cncf.io>
2021-10-13 07:07:59 +0000

    One more update
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- datalake-cron.sh
+++ datalake-cron.sh
@@ -5,7 +5,7 @@ then
 fi
 cd "${GOPATH}/src/github.com/LF-Engineering/es-company-report" || exit 1
 git pull || exit 2
-make || exit 3
+make report || exit 3
 date >> prod_datalake.log
 echo "---------------------------------" >> prod_datalake.log
 INCREMENTAL=1 REPORT=datalake MAX_THREADS=4 DATASOURCES='git,github-issue,gerrit,jira,bugzilla,bugzillarest,confluence' NAME_PREFIX=prod ./run.sh prod | tee -a prod_datalake.log
